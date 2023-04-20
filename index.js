9a04ef14a688f5b4529ffba1c8127b9d126adf20
Helder Pereira <phelder@gmail.com>
2023-04-20 19:33:13 +0200

    added some dummy data

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 3000;
 app.get("/", function(req, res) {
     res.json({
         status: "OK",
-        data: "My Awesome REST API data!!!!!"
+        data: {
+            message: "My Awesome REST API data!!!!!",
+            content: [23, 12, 34, 2, 3, 43, 34, 34, 23]
+        }
     });
 });
 
